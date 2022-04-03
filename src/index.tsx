64e68aeaaec31e46d1a0870cd4a6b319b7408545
siseko <s.nomavila@gmail.com>
2022-04-03 08:49:57 +0200

    folder cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import * as ReactDOMClient from "react-dom/client";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const container = document.getElementById("root");
+
+if (!container) throw new Error("Failed to find container element");
+
+const root = ReactDOMClient.createRoot(container);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
